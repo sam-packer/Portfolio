38e15852b2f08f58de55c6a1b812fefd410b4a78
sam-packer <me@sampacker.com>
2025-03-08 00:03:45 -0600

    Minor fixes for light theme

--- src/components/TimeLine.svelte
+++ src/components/TimeLine.svelte
@@ -6,7 +6,7 @@
 <div class="flex flex-col my-4 first:mt-0 last:mb-0">
     <div class="flex">
         <div class="education__time">
-            <span class="w-4 h-4 bg-primary block rounded-full mt-1.5 shadow-lg shadow-black"></span>
+            <span class="w-4 h-4 bg-primary block rounded-full mt-1.5 shadow-sm dark:shadow-lg shadow-black"></span>
             <span class="education__line bg-primary/50 block h-full w-[2px] translate-x-[7px]"></span>
         </div>
         <div class="experience__data bd-grid px-5">
